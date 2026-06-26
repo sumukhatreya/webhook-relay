5f403d4fa79c7409567e0d99258f660cb6beec7d
Sumukh Atreya <atreya.sumukh@gmail.com>
2026-06-26 17:44:53 +0530

    Refactored application routing

--- backend/app/main.py
+++ backend/app/main.py
@@ -1,12 +1,12 @@
 from fastapi import FastAPI
 
-from app.api.routes import health
+from app.api.routes import router
 from app.core.config import settings
 
 
 def create_app() -> FastAPI:
     app = FastAPI(title=settings.APP_NAME)
-    app.include_router(health.router)
+    app.include_router(router)
     return app
 
 
